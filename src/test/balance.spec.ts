9babd7c0a7a95ccd23f02228ecf700e8fc8dad21
Martin Schere <martinschere@Martins-MacBook-Pro.local>
2024-03-29 11:44:32 +0100

    fix: decodeBalance

--- src/test/balance.spec.ts
+++ src/test/balance.spec.ts
@@ -1,8 +1,11 @@
 import { parseBalance } from '../utils';
 
-describe('Balance', () => {
-  it('Parses correctlyl', () => {
+describe('Batcher tests', () => {
+  it('Tx building cases', () => {
     const result = parseBalance('1a94b130b6');
     expect(result).toBe('2494640310');
+
+    const result2 = parseBalance('1b00000001dc87463b');
+    expect(result2).toBe('7994820155');
   });
 });
--- src/utils.ts
+++ src/utils.ts
@@ -5,10 +5,11 @@ export const fromHex = (hexString: string) => Buffer.from(hexString, 'hex');
 export const parseBalance = (balance: string) => {
   const decodedBalance = decode(fromHex(balance));
 
-  const lovelace =
-    typeof decodedBalance === 'number'
-      ? decodedBalance.toString()
-      : (decodedBalance[0] ?? 0).toString();
-
-  return lovelace;
+  if (typeof decodedBalance === 'bigint') {
+    return decodedBalance.toString();
+  }
+  if (typeof decodedBalance === 'number') {
+    return decodedBalance.toString();
+  }
+  return (decodedBalance[0] ?? 0).toString();
 };
